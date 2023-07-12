7afc92b5844ad155fb2e8e819376aa502e6113e3
Rewat Singh <69641504+REWATSING@users.noreply.github.com>
2023-07-12 17:15:53 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,11 +1,18 @@
 package com.example;
 
-public class Main {
-    public static void main(String[] args) {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int x = 5;
         int y = 0;
         int result = x / y;
-        System.out.println("Result: " + result);
+        resp.getWriter().println("Result: " + result);
     }
 }
-
